495e15c759ef09c762c04c5cca294a4dd5d08bfd
4rnol <arnol01233@gmail.com>
2021-04-18 16:12:58 -0400

    navbar and registerPublication page v.1

---
+++ src/pages/login.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Login = (props) => {
+  return (
+    <div>
+        asedasdsad
+    </div>
+  );
+};
+
+export default Login;
--- src/router/RouterMain.js
+++ src/router/RouterMain.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { routes } from "./RoutesConstants";
 import { BrowserRouter, Route } from "react-router-dom";
+import Login from '../pages/Login';
+import Navbar from '../components/appBar/NavBar';
+import registerPublication from '../pages/publicaciones/RegistrarPublicacion'
 
 const RouterMain = (props) => {
   return (
     <BrowserRouter>
+      <Navbar />
       <Route exact={true} path={"/"} component={""} />
-      <Route exact={true} path={routes.login} component={""} />
+      <Route exact={true} path={routes.login} component={Login} />
+      <Route exact={true} path={routes.registerPublication} component={registerPublication} />
     </BrowserRouter>
   );
 };
