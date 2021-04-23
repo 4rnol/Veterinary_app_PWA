27f4f2cc503765f539e4399a550d9333231567f2
4rnol <arnol01233@gmail.com>
2021-04-22 23:29:03 -0400

    navbar integrated

--- src/pages/registro/RegistrarVeterinario.js
+++ src/pages/registro/RegistrarVeterinario.js
@@ -1,10 +1,29 @@
 import * as React from 'react';
 import "./RegistrarVeterinario.css";
+import {useFiles} from '../../utils/validations/formValidations/useFiles';
+import {projectStorage} from '../../api/Firebase/config';
+
 const RegistrarVeterinario = (props) => {
+
+  const[file,handleFileChange,fileError,
+    setFileError,fileMessage,setFileMessage,previewSource]=useFiles();
+
+  const registrar=()=>{
+    const storageRef = projectStorage.ref(file.name);
+    storageRef.put(file).on('state_changed', () => {
+    }, (err) => {
+      console.log('error' + err);
+    }, async () => {
+      await storageRef.getDownloadURL()
+        .then((res) => {
+          console.log(res);
+        });
+    });
+  };
   return (
     <div className="register-section">
     <div className="register-box">
-      <div id="scroll"> 
+      <div id="scroll">       
         <h1>Registrar Veterinaria</h1>        
           <br />
           <label htmlFor="username">Nombre Veterinaria</label>
@@ -33,7 +52,18 @@ const RegistrarVeterinario = (props) => {
           <br />
           <label htmlFor="username">WhatsApp</label>
           <input type="text" placeholder="Ingrese WhatsApp"/>
-          <input type="submit" value="Registrar"/>
+          <label htmlFor="username">Agregar Imagen</label>
+          <input type="file" placeholder="Ingrese imagen" onChange={({target})=>handleFileChange(target.files[0])}/>
+
+          <iframe
+              desciption="iframe"
+              data="application"
+              src={previewSource}
+          />
+
+          <button type="submit" onClick={()=>registrar()}>
+            Registrar
+            </button>
           </div>
       </div>
       </div>
