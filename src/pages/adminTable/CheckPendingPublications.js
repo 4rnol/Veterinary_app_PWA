cef1f551736cc0e04e09b5f0baefcea7e9ce2eed
Arnol Brandon Flores Rodriguez <arnol01233@gmail.com>
2021-06-09 11:40:40 -0400

    admin

--- src/pages/adminTable/CheckPendingPublications.js
+++ src/pages/adminTable/CheckPendingPublications.js
@@ -1,14 +1,12 @@
 import * as React from 'react';
+import './styles.css';
 
-const CheckPendingPublications=()=>{
-    return(
-    <>
-    <h1>
-
-        Tables
-    </h1>
-    </>
-    );
-}
+const CheckPendingPublications = () => {
+  return (
+    <div className="checkPub-section">
+      <h1>Tables</h1>
+    </div>
+  );
+};
 
 export default CheckPendingPublications;
